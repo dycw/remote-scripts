b25a0cd9f726b68f844f70435f7e8d0358f6071f
Derek Wan <d.wan@icloud.com>
2025-09-11 08:40:21 +0900

    Add `run-test-hook` (#28)

---
+++ run-test-hook.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# add hook to `.pre-commit-hooks.yaml`
+# edit `HOOK_NAME` below
+# in your project, run:
+#     ~/work/remote-scripts/run-test-hook.sh
+
+PATH_DIR="$(
+	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit
+	pwd -P
+)"
+HOOK_NAME='check-submodules-updated'
+
+pre-commit try-repo --verbose --all-files "${PATH_DIR}" "${HOOK_NAME}"
