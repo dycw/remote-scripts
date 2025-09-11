e5c92ba538b57504dc2db83191fbd36654182669
Derek Wan <d.wan@icloud.com>
2025-09-11 10:00:42 +0900

    2025-09-11 10:00:41 (Thu) > DW-Mac > derekwan

--- run-test-hook.sh
+++ run-test-hook.sh
@@ -9,6 +9,7 @@ PATH_DIR="$(
 	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit
 	pwd -P
 )"
-HOOK_NAME='check-submodules-updated'
+# HOOK_NAME='check-submodules-updated'
+HOOK_NAME='check-version-bumped'
 
 pre-commit try-repo --verbose --all-files "${PATH_DIR}" "${HOOK_NAME}"
